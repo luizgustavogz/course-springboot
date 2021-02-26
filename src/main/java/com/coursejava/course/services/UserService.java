ca52659bf9f44ae841563c7fd909ce9dbab67cca
Luiz Gustavo <luiz.gustavogz02@gmail.com>
2021-02-26 11:22:28 -0300

    User delete

--- src/main/java/com/coursejava/course/services/UserService.java
+++ src/main/java/com/coursejava/course/services/UserService.java
@@ -23,8 +23,12 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
-		return repository.save(obj);	
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
 	}
 }
